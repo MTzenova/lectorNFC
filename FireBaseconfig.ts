13f1a1d8424716ce69fb8b3c09b73f88fe2d6a7a
Chaima <1925046@alu.murciaeduca.es>
2025-02-16 17:37:37 +0100

    metida configuracion de firebase

---
+++ FireBaseconfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBY_iUPeAJpPnG_zVvA04OCflLqoI4MvRg",
+  authDomain: "lectornfcdatos.firebaseapp.com",
+  projectId: "lectornfcdatos",
+  storageBucket: "lectornfcdatos.firebasestorage.app",
+  messagingSenderId: "702174471222",
+  appId: "1:702174471222:web:c18b38a87e1a21b4626497"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
